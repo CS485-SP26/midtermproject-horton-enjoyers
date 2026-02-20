bdc17d9b2bd5a723c1fcbaf659534d08159ce6cd
Kabire-A <160642553+Kabire-A@users.noreply.github.com>
2026-02-20 14:02:09 -0800

    Scene 1+2 Overhaul
    - Change building used for shop
    - Make interior of shop (Scene 2)
    - Adjusted some GUI (canvas)
    - Updated Credits Attribution

--- Assets/Art/Art Credits.cs
+++ Assets/Art/Art Credits.cs
@@ -10,4 +10,19 @@ public class ArtCredits
 
     // YughuesFreeGroundMaterials
     // https://assetstore.unity.com/packages/2d/textures-materials/nature/yughues-free-ground-materials-13001
+
+    // Mega Fantasy Props Pack
+    // https://assetstore.unity.com/packages/3d/environments/fantasy/mega-fantasy-props-pack-87811#description
+
+    // Medieval Buildings Modular
+    // https://assetstore.unity.com/packages/3d/environments/historic/medieval-buildings-modular-176829
+
+    // Stylized Potted Plants
+    // https://assetstore.unity.com/packages/3d/vegetation/plants/stylized-potted-plants-290047
+
+    // Garden Realistic Tools
+    // https://assetstore.unity.com/packages/3d/garden-realistic-tools-68960
+
+    // Old Water Well
+    // https://assetstore.unity.com/packages/3d/environments/old-water-well-138903
 }
